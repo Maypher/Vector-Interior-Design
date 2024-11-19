3f0a94532c413df407f2faffa544b621571c70be
Maypher <maypheroffcial@gmail.com>
2024-11-19 15:07:46 -0300

    Remove fetching resources from backend

--- backend/admin/src/resources/routes.py
+++ backend/admin/src/resources/routes.py
@@ -5,32 +5,6 @@ from auth.authentication import login_required
 obra_routes = Blueprint("resource_creation", __name__, url_prefix="/obras")
 
 
-@obra_routes.get("/")
-def get_obras():
-    try:
-        page = request.args.get("pagina", 1)
-        # This is done because if page is an empty parameter
-        # it gets saved as "" not causing the raise and getting
-        # passed to the function as is.
-        if page:
-            page = int(page)
-        else:
-            raise ValueError
-    except ValueError:
-        return "Invalid page number.", 400
-
-    obras = obra.get_obras(page)
-
-    return jsonify(obras)
-
-
-@obra_routes.get("/<int:id>")
-def get_obra(id: int):
-    found_obra = obra.get_obra_by_id(id)
-
-    return (jsonify(found_obra), 200) if found_obra else ("", 404)
-
-
 @obra_routes.post("/crear")
 @login_required
 def new_work():
