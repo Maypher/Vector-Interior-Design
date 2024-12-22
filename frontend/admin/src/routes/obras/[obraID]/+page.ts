0fb53a3ef6aef594167e5a84dbb59093f62d451b
Maypher <maypheroffcial@gmail.com>
2024-12-22 20:05:42 -0300

    Fix bug where public status wouldn't be returned by obra query

--- frontend/admin/src/routes/obras/[obraID]/+page.ts
+++ frontend/admin/src/routes/obras/[obraID]/+page.ts
@@ -16,6 +16,7 @@ export async function load({ params, fetch }) {
                         name
                         description
                         area
+                        public
                         ambientes {
                             id
                             name
