6c0a15e5b0c79ca0854d64c96e8d179908b5351b
Maypher <maypheroffcial@gmail.com>
2024-12-24 15:34:31 -0300

    Add sortable list to admin main page (only visual, no functionality yet)

---
+++ frontend/admin/src/lib/components/input/SortableList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Sortable, { type Options } from 'sortablejs';
+	import { onMount, type Snippet } from 'svelte';
+
+	let {
+		sortable = $bindable(),
+		sortableOptions,
+		sortableId,
+		children
+	}: {
+		sortable?: Sortable;
+		sortableOptions?: Options;
+		sortableId: string;
+		children: Snippet<[]>;
+	} = $props();
+
+	onMount(() => {
+		const element = document.getElementById(sortableId) as HTMLElement;
+		sortable = Sortable.create(element, sortableOptions);
+	});
+</script>
+
+<div>
+	{@render children()}
+</div>
--- frontend/admin/src/routes/obras/+page.svelte
+++ frontend/admin/src/routes/obras/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount, untrack } from 'svelte';
 	import graphql from '$lib/utilities/api';
 	import { PUBLIC_imageURL } from '$env/static/public';
+	import SortableList from '$lib/components/input/SortableList.svelte';
+	import Sortable, { type Options } from 'sortablejs';
+	import '$lib/styles/ordenableList.css';
 
 	let searchParams = $state({
 		name: '',
@@ -9,13 +12,29 @@
 	});
 
 	// Used to highlight the search string in the results. Not derived because it should only update on search.
-	let nameRegex: RegExp = $state(new RegExp(`(${searchParams.name})`, 'gi'));
+	let nameRegex: RegExp = $derived(new RegExp(`(${searchParams.name})`, 'gi'));
 
-	let obraPromise: Promise<any> | undefined = $state(undefined);
+	let obraPromise: Promise<any> | undefined = $derived.by(() => fetchObras());
+
+	let sortable: Sortable = $state()!;
+	let sortableEnabled: boolean = $derived(searchParams.name === '');
+
+	$effect(() => {
+		const searchQuery = searchParams;
+		untrack(() => {
+			sortable?.option('disabled', searchQuery.name !== '');
+		});
+	});
+
+	const sortableOptions: Options = {
+		handle: '.handle',
+		draggable: '.item',
+		chosenClass: 'item-chosen',
+		ghostClass: 'item-ghost',
+		animation: 150
+	};
 
 	async function fetchObras() {
-		nameRegex = new RegExp(`(${searchParams.name})`, 'gi');
-
 		const query = `
 			query GetObras($page: Int, $name: String) {
 				obras(page: $page, name: $name) {
@@ -24,6 +43,7 @@
 					obras {
 						id
 						name
+						public
 						thumbnail {
 							filename
 							altText
@@ -34,18 +54,12 @@
 		`;
 		return (await graphql(query, searchParams)).obras;
 	}
-
-	const onclick = () => (obraPromise = fetchObras());
-
-	onMount(() => onclick());
 </script>
 
 <div class="bg-red-400 min-h-screen p-4">
 	<div class="bg-gray-500 p-2 flex justify-between items-center rounded-lg max-w-md m-auto">
 		<input type="text" class="bg-transparent outline-none w-full" bind:value={searchParams.name} />
-		<button class="material-symbols-outlined border-l-2 border-gray-700 pl-3" {onclick}>
-			search
-		</button>
+		<span class="material-symbols-outlined border-l-2 border-gray-700 pl-3"> search </span>
 	</div>
 	<div class="my-4 max-w-md m-auto">
 		{#if obraPromise}
@@ -55,27 +69,61 @@
 				{#if data.obras.length == 0}
 					{`Ninguna obra encontrada.`}
 				{:else}
-					<ul class="list-disc">
-						{#each data.obras as obra}
-							<li class="bg-green-300 my-1">
-								<a href={`/obras/${obra.id}`} class="block w-full hover:bg-gray-600">
-									{@html obra.name.replace(nameRegex, '<b>$1</b>')}
-									{#if obra.thumbnail}
-										<img
-											src={`${PUBLIC_imageURL}${obra.thumbnail.filename}`}
-											alt={obra.thumbnail.alt_text}
-										/>
+					<SortableList bind:sortable {sortableOptions} sortableId="sortable">
+						<div id="sortable">
+							{#each data.obras as obra (obra.id)}
+								<div class="item flex items-center">
+									{#if sortableEnabled}
+										<span
+											class="material-symbols-outlined handle border-r-2 p-2 h-full content-center border-gray-800 hover:cursor-pointer"
+										>
+											drag_indicator
+										</span>
 									{/if}
-								</a>
-							</li>
-						{/each}
-					</ul>
+									<a
+										href={`/obras/${obra.id}`}
+										class="size-full flex justify-between items-center pl-2 hover:bg-yellow-600"
+									>
+										<p>
+											{@html obra.name.replace(nameRegex, '<b>$1</b>')}
+										</p>
+										{#if obra.thumbnail}
+											<img
+												src={`${PUBLIC_imageURL}${obra.thumbnail?.filename}`}
+												alt={obra.thumbnail?.altText}
+												class="max-h-full"
+											/>
+										{/if}
+									</a>
+								</div>
+							{/each}
+						</div>
+					</SortableList>
+					<a
+						href="/obras/crear"
+						class="block bg-amber-400 hover:bg-amber-600 w-full text-center p-2 border-2 border-black sticky bottom-0"
+						>Nueva Obra</a
+					>
 				{/if}
-				<div><p>{data.page}/{data.pageCount}</p></div>
+				<div class="flex gap-3 w-fit m-auto my-2 text-lg items-center">
+					{#if searchParams.page > 1}
+						<button
+							class="rounded-lg p-1 hover:font-bold hover:bg-gray-300"
+							onclick={() => {
+								searchParams.page--;
+							}}>&lt</button
+						>
+					{/if}
+					<p>{data.page}/{data.pageCount}</p>
+					{#if searchParams.page < data.pageCount}
+						<button
+							class="rounded-lg p-1 hover:font-bold hover:bg-gray-300"
+							onclick={() => searchParams.page++}
+							>&gt
+						</button>
+					{/if}
+				</div>
 			{/await}
-			<a href="/obras/crear" class="block bg-green-200 w-fit p-2 m-1 rounded-lg hover:bg-violet-700"
-				>Nueva Obra</a
-			>
 		{/if}
 	</div>
 </div>
