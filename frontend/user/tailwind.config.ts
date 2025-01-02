a23127d7c1b52034eb52cc12131d7498c1d6f20a
Maypher <maypheroffcial@gmail.com>
2025-01-02 19:01:25 -0300

    Add arial font to tailwind

--- frontend/user/tailwind.config.ts
+++ frontend/user/tailwind.config.ts
@@ -6,8 +6,9 @@ export default {
 	theme: {
 		extend: {
 			fontFamily: {
-				"Agency-FB": ["Agency-FB", "Arial", "sans-serif"],
-				"Bahnschrift": ["Bahnschrift", "Arial", "sans-serif"],
+				"Agency-FB": ["Agency-FB", "Helvetica", "sans-serif"],
+				"Bahnschrift": ["Bahnschrift", "Helvetica", "sans-serif"],
+				"Arial": ["Arial", "Helvetica", "sans-serif"],
 			},
 			colors: {
 				"vector-grey": "#e6e6e6",
