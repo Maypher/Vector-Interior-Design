cb4666a606c619bf941d07b0f2351bcf341f9b21
Maypher <maypheroffcial@gmail.com>
2025-01-03 14:35:52 -0300

    Improve file selection in image creation UI

--- frontend/admin/src/routes/obras/[obraID]/ambientes/[ambienteID]/imagenes/crear/+page.svelte
+++ frontend/admin/src/routes/obras/[obraID]/ambientes/[ambienteID]/imagenes/crear/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { setError, superForm } from 'sveltekit-superforms';
+	import { superForm } from 'sveltekit-superforms';
 	import type { PageData } from './$types';
 	import { yup } from 'sveltekit-superforms/adapters';
 	import { imageUpdateSchema } from '$lib/utilities/yupSchemas';
@@ -8,9 +8,11 @@
 	import { error } from '$lib/utilities/toasts';
 	import { goto } from '$app/navigation';
 	import Errors from '$lib/components/input/Errors.svelte';
+	import { preventDefault } from 'svelte/legacy';
 
 	const { data }: { data: PageData } = $props();
-	let inputFile: FileList | undefined | null = $state();
+	let inputFile: File | undefined | null = $state();
+	$inspect(inputFile);
 	let fileErrors: Array<string> = $state([]);
 	let submitting: boolean = $state(false);
 
@@ -22,14 +24,13 @@
 			submitting = true;
 
 			if (createForm.valid) {
-				const file = inputFile?.item(0);
-				if (!file) {
+				if (!inputFile) {
 					fileErrors.push('Imagen requerida.');
 					submitting = false;
 					return;
 				}
 
-				if (file.size > 1e7) {
+				if (inputFile.size > 1e7) {
 					fileErrors.push('Imagen debe ser menor a 10 Mb.');
 					submitting = false;
 					return;
@@ -73,7 +74,7 @@
 
 				formData.append('operations', operations);
 				formData.append('map', map);
-				formData.append('file', file);
+				formData.append('file', inputFile);
 
 				const res = await fetch(PUBLIC_graphql, {
 					method: 'POST',
@@ -105,12 +106,79 @@
 			submitting = false;
 		}
 	});
+
+	function ondragenter(event: DragEvent) {
+		event.preventDefault();
+		document.getElementById('dropzone')!.classList.add('hovering');
+	}
+
+	function ondragleave(event: DragEvent) {
+		event.preventDefault();
+
+		const dropzone = document.getElementById('dropzone')!;
+		const childElement = event.relatedTarget;
+		// Doing this because it triggers when entering a child element.
+		// This way it doesn't remove the class when entering a child element.
+		if (childElement && !dropzone.contains(childElement as Node))
+			document.getElementById('dropzone')!.classList.remove('hovering');
+	}
+
+	function ondrop(event: DragEvent) {
+		event.preventDefault();
+		document.getElementById('dropzone')!.classList.remove('hovering');
+
+		const file = event.dataTransfer?.files.item(0);
+		if (file) inputFile = file;
+	}
 </script>
 
 <div class="m-5">
 	<form use:enhance class="bg-amber-600 max-w-xl m-auto p-3">
 		<fieldset disabled={submitting}>
-			<input type="file" accept="image/png, image/jpeg" bind:files={inputFile} />
+			<input
+				type="file"
+				class="hidden"
+				id="imageInput"
+				accept="image/png, image/jpeg"
+				onchange={(e) => (inputFile = (e.target as HTMLInputElement).files?.item(0))}
+			/>
+
+			<div
+				id="dropzone"
+				class="flex justify-center items-center border-dashed border-2 mt-10 min-h-64 rounded-md"
+				class:w-full={inputFile}
+				role="region"
+				{ondragenter}
+				{ondragleave}
+				{ondrop}
+				ondragover={(e) => e.preventDefault()}
+			>
+				{#if inputFile}
+					<div class="relative my-2">
+						<button
+							class="absolute top-0 right-0 p-1 rounded-bl-md transition-colors text-red-700 hover:text-black hover:bg-red-700"
+							type="button"
+							onclick={() => (inputFile = null)}
+						>
+							<span class="material-symbols-outlined"> close </span>
+						</button>
+						<img
+							src={URL.createObjectURL(inputFile)}
+							alt="Previsualización"
+							class="max-h-80 m-auto"
+						/>
+					</div>
+				{:else}
+					<p>
+						Arrastre o <label
+							for="imageInput"
+							class="text-blue-100 hover:pb-0.5 hover:cursor-pointer border-b-2 transition-all"
+							>seleccione un archivo</label
+						>.
+					</p>
+				{/if}
+			</div>
+
 			<Errors errors={fileErrors} />
 			<TextInput
 				name="altText"
@@ -124,3 +192,9 @@
 		</fieldset>
 	</form>
 </div>
+
+<style>
+	:global(#dropzone.hovering) {
+		background-color: rgba(0, 0, 0, 0.5);
+	}
+</style>
