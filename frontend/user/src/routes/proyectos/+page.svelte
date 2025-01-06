d4fb5facef7687cb6f22800ef7fe1f1cad5bf0fb
Maypher <maypheroffcial@gmail.com>
2025-01-06 10:40:41 -0300

    Add scroll arrow to projects page

--- frontend/user/src/routes/proyectos/+page.svelte
+++ frontend/user/src/routes/proyectos/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
 	import { PUBLIC_imagesUrl } from '$env/static/public';
 	import logo from '$lib/images/logo.svg';
+	import { onMount } from 'svelte';
 	import type { PageData } from './$types';
 
 	const { data }: { data: PageData } = $props();
 	const obras = data.obras;
+
+	onMount(() => {
+		const arrow = document.getElementById('arrow')!;
+		arrow.addEventListener('transitionend', (e: TransitionEvent) => {
+			arrow.classList.add('animate');
+			e.target?.removeEventListener('transitionend', null);
+		});
+
+		setTimeout(() => {
+			arrow.classList.add('showing');
+		}, 3000);
+
+		const arrowLimit = document.getElementById('arrowLimit');
+
+		if (arrowLimit) {
+			const observer = new IntersectionObserver((entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) {
+						if (arrow.classList.contains('showing')) {
+							arrow.classList.remove('showing');
+							arrow.classList.remove('hiding');
+							setTimeout(() => arrow.remove(), 100);
+						} else arrow.remove();
+
+						observer.disconnect();
+					}
+				});
+			});
+
+			observer.observe(arrowLimit);
+		}
+	});
 </script>
 
 <header class="bg-vector-grey absolute top-0 z-10 flex justify-center p-5">
@@ -24,6 +57,9 @@
 						<p class="font-Agency-FB text-[1.7rem]">{obra.name}</p>
 					</a>
 				</div>
+				{#if i === 1}
+					<div id="arrowLimit" inert></div>
+				{/if}
 			</div>
 		{/if}
 		{#if i === 0}
@@ -35,13 +71,41 @@
 <style>
 	#arrow {
 		position: absolute;
-		bottom: 2%;
 		left: 50%;
+		opacity: 0;
+		bottom: 10%;
 		transform: translate(-50%, 0) rotate(45deg);
 		height: 20px;
 		width: 20px;
 		border-bottom: 5px solid white;
 		border-right: 5px solid white;
 		border-radius: 2px;
+		transition: all 2s ease-out;
+	}
+
+	:global #arrow.showing {
+		opacity: 1;
+		bottom: 5%;
+	}
+
+	:global #arrow.hiding {
+		transition: opacity 0.1s;
+		opacity: 0;
+	}
+
+	:global #arrow.animate {
+		animation-name: arrowHover;
+		animation-duration: 1s;
+		animation-direction: alternate;
+		animation-iteration-count: infinite;
+	}
+
+	@keyframes arrowHover {
+		from {
+			bottom: 5%;
+		}
+		to {
+			bottom: 3%;
+		}
 	}
 </style>
