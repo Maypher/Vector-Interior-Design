1f26dbed05880d9c38cd766bc1503586527b8271
Maypher <maypheroffcial@gmail.com>
2025-01-29 12:39:38 -0300

    Hide overflow-x in mobile editor

--- frontend/admin/src/routes/obras/paginaPrincipal/movil/+page@.svelte
+++ frontend/admin/src/routes/obras/paginaPrincipal/movil/+page@.svelte
@@ -370,7 +370,7 @@
 			</p>
 		</header>
 		<div
-			class="bg-black md:border-2 md:border-white md:aspect-[9/16] w-full md:w-sm flex flex-col overflow-y-scroll relative"
+			class="bg-black md:border-2 md:border-white md:aspect-[9/16] w-full md:w-sm flex flex-col overflow-y-scroll overflow-x-clip relative"
 		>
 			<div
 				class="bg-black z-10 h-10 shrink-0 md:border-white md:border-2 w-full hidden md:flex sticky top-0 justify-center items-center"
