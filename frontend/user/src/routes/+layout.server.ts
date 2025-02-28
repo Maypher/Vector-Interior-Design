86e19863f050a7e45b4117eb86cbd7b95c2d7ac5
Maypher <maypheroffcial@gmail.com>
2025-02-28 17:58:21 -0300

    Fix bug with setting language cookie

--- frontend/user/src/routes/+layout.server.ts
+++ frontend/user/src/routes/+layout.server.ts
@@ -1,16 +1,14 @@
 export const load = ({ cookies, request }) => {
     const availableLanguages = ["en", "es"];
 
-    let selectedLanguage = cookies.get('lang') || 'en';
+    let selectedLanguage = cookies.get('lang');
 
     if (!selectedLanguage) {
         const acceptedLanguages = request.headers.get("Accept-Language")?.split(',');
         if (acceptedLanguages) {
             const preferredLang = acceptedLanguages.find((x) => availableLanguages.includes(x.slice(0, 2)))?.slice(0, 2) || 'en';
             selectedLanguage = preferredLang;
-        }
-
-        cookies.set('lang', selectedLanguage, { path: '/', httpOnly: false });
+        } else selectedLanguage = 'en';
     }
 
 
