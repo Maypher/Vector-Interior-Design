c319b20b4671af1a5dc627367ed4bd3ad2dd0c76
Maypher <maypheroffcial@gmail.com>
2025-03-04 13:52:57 -0300

    Change some messages in conclusion

--- frontend/user/src/routes/[lang]/proyectos/conclusion/+page.svelte
+++ frontend/user/src/routes/[lang]/proyectos/conclusion/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import logo from '$lib/images/logo.svg';
-	import pencil from '$lib/images/pencil.svg';
 	import { getI18n } from '$lib/i18n';
+	import mdToHtml from '$lib/utilities/markdown';
 
 	const i18n = getI18n();
 </script>
@@ -9,27 +9,11 @@
 <div
 	class="bg-vector-grey flex h-screen w-screen flex-col items-center justify-center gap-20 overflow-x-clip p-10"
 >
-	<a href="/" class="transition-transform hover:scale-110">
+	<a href={`/${$i18n.language}`} class="transition-transform will-change-transform hover:scale-110">
 		<img src={logo} alt="Vector: Interior Design" class="w-64" />
 	</a>
-	<div class="flex flex-col items-center gap-20 text-center text-xl">
-		<p class="relative z-10 md:whitespace-pre-wrap">
-			{$i18n.t('conclusionMain')}
-		</p>
-		<p>
-			{$i18n.t('conclusionSecondary')}
-			<b class="relative block md:inline">
-				{$i18n.t('slogan')}<b class="text-vector-orange ml-1">.</b>
-
-				<img
-					src={pencil}
-					alt="pencil"
-					class="size-70 rotate-20 absolute bottom-3 right-0 origin-bottom translate-x-1/2"
-				/>
-			</b>
-		</p>
+	<h1 class="text-4xl"><b>{$i18n.t('conclusionHeader')}</b></h1>
+	<div id="conclusion" class="text-justify indent-5 text-xl" style="text-align-last: justify;">
+		{@html mdToHtml($i18n.t('conclusionBody'))}
 	</div>
 </div>
-
-<style>
-</style>
