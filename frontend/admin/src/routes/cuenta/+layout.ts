0127cfe9b4287bb3dc5ef3a3ef3db0ecac047b25
Maypher <maypheroffcial@gmail.com>
2025-03-10 13:16:15 -0300

    Delete unused pages

--- frontend/admin/src/routes/cuenta/+layout.ts
+++
@@ -1,9 +0,0 @@
-import { PUBLIC_apiURL } from '$env/static/public';
-
-export async function load({ fetch }) {
-    let res = await fetch(`${PUBLIC_apiURL}auth/usuario-creado`);
-
-    let user_count = parseInt(await res.text());
-
-    return { user_exists: user_count > 0 }
-}
--- frontend/admin/src/routes/cuenta/+page.ts
+++ frontend/admin/src/routes/cuenta/+page.ts
@@ -1,13 +1,8 @@
-import { redirect } from '@sveltejs/kit';
 import { superValidate } from "sveltekit-superforms";
 import { yup } from "sveltekit-superforms/adapters";
 import { loginSchema } from '$lib/utilities/yupSchemas.js';
 
-export async function load({ parent }) {
-    let user_exists = (await parent()).user_exists;
-
-    if (!user_exists) redirect(302, "/cuenta/crear/");
-
+export async function load() {
     const loginForm = await superValidate(yup(loginSchema));
 
     return { loginForm };
