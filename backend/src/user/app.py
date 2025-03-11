774fdc66f0a95dc157a2373b3de78babed3b6210
Maypher <maypheroffcial@gmail.com>
2025-03-11 16:38:10 -0300

    Change user backend name

--- backend/src/user/app.py
+++ backend/src/user/app.py
@@ -12,7 +12,7 @@ import os
 
 
 def create_app(ctx=Context()) -> Sanic:
-    app = Sanic("TQ-admin-backend", ctx=ctx)
+    app = Sanic("TQ-user-backend", ctx=ctx)
 
     app.add_route(
         UserGraphQLView.as_view(schema=Schema(query=Query)),
