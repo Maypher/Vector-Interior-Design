dd18649348373dbcf5a03a02890acf32579ec446
Maypher <maypheroffcial@gmail.com>
2025-03-26 12:48:00 -0300

    Tweak css

--- frontend/admin/src/routes/obras/[obraID]/+page.svelte
+++ frontend/admin/src/routes/obras/[obraID]/+page.svelte
@@ -174,7 +174,7 @@
 	}
 </script>
 
-<div class="bg-vector-black py-5 lg:flex gap-x-10 lg:px-10 overflow-y-hidden lg:header-screen">
+<div class="bg-vector-black py-5 lg:flex gap-x-10 lg:px-10 overflow-y-hidden header-screen">
 	<!-- Name, area and description -->
 	<form
 		class="bg-vector-cream/50 p-4 lg:rounded-lg flex flex-col justify-stretch gap-y-20 lg:gap-y-10"
