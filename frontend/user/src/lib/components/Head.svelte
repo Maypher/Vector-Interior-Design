87f7162fb9cd565cce2a923b591033502f19430d
Maypher <maypheroffcial@gmail.com>
2025-03-30 11:06:33 -0400

    Transform og description from md to plain text

--- frontend/user/src/lib/components/Head.svelte
+++ frontend/user/src/lib/components/Head.svelte
@@ -12,7 +12,31 @@
 		imageUrl?: string;
 	}
 
-	const { url, title, alternateEn, alternateEs, ogTitle, description, imageUrl }: Props = $props();
+	let {
+		url,
+		title,
+		alternateEn,
+		alternateEs,
+		ogTitle,
+		description,
+		imageUrl,
+		ogDescription
+	}: Props = $props();
+
+	// Doing this to remove all markdown syntax in description
+	ogDescription = ogDescription
+		.replace(/(\*\*|__)(.*?)\1/g, '$2') // Bold (**text** or __text__)
+		.replace(/(`{1,3})(.*?)\1/g, '$2') // Inline code (`code` or ```code```)
+		.replace(/!\[.*?\]\(.*?\)/g, '') // Images (![alt](url))
+		.replace(/\[(.*?)\]\(.*?\)/g, '$1') // Links ([text](url))
+		.replace(/^> /gm, '') // Blockquotes (> quote)
+		.replace(/^#{1,6}/gm, '') // Headings (# H1, ## H2, etc.)
+		.replace(/^\* |\d+\.\s/gm, '') // Lists (- item, 1. item)
+		.replace(/~~(.*?)~~/g, '$1') // Strikethrough (~~text~~)
+		.replace(/`/g, '') // Inline backticks (`code`)
+		.replace(/---/g, '') // Horizontal rules (---)
+		.replace(/\|.*?\|/g, '') // Tables (| col1 | col2 |)
+		.trim();
 </script>
 
 <svelte:head>
@@ -27,7 +51,7 @@
 	<meta property="og:url" content={url} />
 	<meta property="og:type" content="website" />
 	<meta property="og:title" content={ogTitle} />
-	<meta property="og:description" content={description} />
+	<meta property="og:description" content={ogDescription} />
 	<meta property="og:image" content={imageUrl} />
 
 	<!-- Twitter Meta Tags -->
@@ -35,6 +59,6 @@
 	<meta property="twitter:domain" content={websiteUrl.replace('https://', '')} />
 	<meta property="twitter:url" content={url} />
 	<meta name="twitter:title" content={ogTitle} />
-	<meta name="twitter:description" content={description} />
+	<meta name="twitter:description" content={ogDescription} />
 	<meta name="twitter:image" content={imageUrl} />
 </svelte:head>
