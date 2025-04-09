186822ecf01538c22910741c6d29009e53008bb8
Maypher <maypheroffcial@gmail.com>
2025-04-09 00:46:50 -0400

    Run migrations and user creation directly in entrypoint

--- backend/src/admin/entrypoint.sh
+++ backend/src/admin/entrypoint.sh
@@ -9,6 +9,26 @@ if [ ! -f "$ADMIN_PASSWORD_PATH" ] || [ ! -s "$ADMIN_PASSWORD_PATH" ]; then
   exit 1
 fi
 
+cd cli
+
+user_count=$(python users.py list | wc -l);
+
+# If no users have been created create a new one with a random password
+if [ "$user_count" -eq 0 ]; then
+  username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 13; echo);
+  email="contact@vectorinterior.design";
+  password=$(tr -dc 'A-Za-z0-9!#$%&\()*+,-./:;<=>?@[\]^_{|}~' </dev/urandom | head -c 13; echo);
+
+  python users.py new -n "$username" -e "$email" -p "$password";
+
+  echo "Default user created successfully. Name: $username Email: $email password: $password";
+fi
+
+
+python migrations.py migrate;
+
+cd ..
+
 if [ $DEV_MODE = 1 ]; then
     sanic app:create_app --host=0.0.0.0 --port=80 --factory --dev
 else
