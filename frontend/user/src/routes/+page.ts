3445ef2c45030e8fb7edb1f1d8b6ec49e65b8070
Maypher <maypheroffcial@gmail.com>
2025-04-09 00:46:07 -0400

    Remove browser check because discord told me to

--- frontend/user/src/routes/+page.ts
+++ frontend/user/src/routes/+page.ts
@@ -5,6 +5,5 @@ import { redirect } from '@sveltejs/kit';
 export const load = async ({ parent }) => {
     const { selectedLanguage } = await parent();
     const redirectTo = `/${selectedLanguage}`;
-    if (browser) await goto(redirectTo);
-    else redirect(302, redirectTo);
+    redirect(302, redirectTo);
 }
