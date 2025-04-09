04a1fd21b5b66949df3551599149b7c62c240b39
Maypher <maypheroffcial@gmail.com>
2025-04-09 14:27:44 -0400

    Add error message to user frontend when backend is down

--- frontend/user/src/routes/+error.svelte
+++ frontend/user/src/routes/+error.svelte
@@ -6,16 +6,20 @@
 </script>
 
 <svelte:head>
-	<title>{$i18n.language === 'es' ? 'Página no encontrada' : 'Page not found.'}</title>
+	<title>{$i18n.language === 'es' ? 'Página no encontrada' : 'Page not found'}</title>
 	<meta name="robots" content="noindex, nofollow" />
 </svelte:head>
 
 <div class="flex h-screen w-screen flex-col items-center justify-center gap-5 text-4xl text-white">
 	<h1 class="font-Noopla text-vector-orange text-[20rem]" style="line-height: 0.8;">X</h1>
-	<a href={`/${$i18n.language}/`} class="font-Noopla">
-		<span id="arrow">&lt;-</span>
+	<a href={`/${$i18n.language}/`} class="font-Noopla text-center">
 		{#if page.status === 404}
+			<span id="arrow">&lt;-</span>
 			{$i18n.language === 'es' ? 'Página no encontrada' : 'Page not found'}
+		{:else if page.status === 502}
+			{$i18n.language === 'es'
+				? 'Servidor temporalmente fuera de servicio. Por favor intentar de nuevo más tarde'
+				: 'Server temporarily down. Please try again later.'}
 		{:else}
 			{page.error?.message}
 		{/if}
