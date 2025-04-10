f566240d270d29bc96e92a8e0042e319ab0a4bf1
Maypher <maypheroffcial@gmail.com>
2025-04-10 14:10:07 -0400

    Add error page to admin frontend

---
+++ frontend/admin/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<div
+	class="header-screen w-full flex items-center justify-center flex-col text-vector-cream text-3xl font-Nexa"
+>
+	<h1 class="text-4xl">{page.status}</h1>
+	<a href="/cuenta" class="underline"
+		>{page.status === 404 ? 'Página no encontrada' : page.error?.message}</a
+	>
+</div>
