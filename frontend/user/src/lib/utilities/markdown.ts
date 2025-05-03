7fc844f32024cb849615df2b07a119be15d68e6c
Maypher <maypheroffcial@gmail.com>
2025-05-02 23:02:41 -0400

    Make markdown replace h tags with p tags with a class as to not confuse web crawlers

--- frontend/user/src/lib/utilities/markdown.ts
+++ frontend/user/src/lib/utilities/markdown.ts
@@ -1,10 +1,22 @@
 import showdown from "showdown";
 
+// Replaces all <h#></h#> tags with <p class="markdown-header markdown-header-#"></p>
+// To not confuse web crawlers when finding multiple header tags
+const headerToParagraph: showdown.ShowdownExtension = {
+    type: "output",
+    regex: /^<h([1-6]) id="(.+)">(.+)<\/h[1-6]>/gm,
+    replace: (_match: string, level: string, id: string, content: string) => {
+        return `<p id="${id}" class="markdown-header markdown-header-${level}">${content}</p>`;
+    }
+}
+
 const converter = new showdown.Converter({
     openLinksInNewWindow: true,
-    simpleLineBreaks: true
+    simpleLineBreaks: true,
+    extensions: [headerToParagraph]
 });
 
 export default function mdToHtml(md: string): string {
+    console.log(md);
     return converter.makeHtml(md);
 }
