a2cd4bdd861492effe84c0f8b8d304f230a5816b
Maypher <maypheroffcial@gmail.com>
2025-05-16 12:15:32 -0400

    Fix issue with admin panel nesting html tags inside one another

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,11 @@
-import React from 'react'
-import './styles.css'
-
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
-}
+import './globals.css'
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   )
 }
