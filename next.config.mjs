3477ae18298c0274175e4136ef420077875211b4
Maypher <maypheroffcial@gmail.com>
2025-05-16 13:21:31 -0400

    Change dev button position

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     imageSizes: [800, 1500, 1920],
   },
+  devIndicators: {
+    position: 'bottom-right',
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
