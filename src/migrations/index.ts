8e18577d0b907f6f1945af36dcec04bccbadb650
Maypher <maypheroffcial@gmail.com>
2025-06-05 14:23:03 -0400

    Add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250601_173010 from './20250601_173010';
+import * as migration_20250605_182240 from './20250605_182240';
 
 export const migrations = [
   {
     up: migration_20250601_173010.up,
     down: migration_20250601_173010.down,
-    name: '20250601_173010'
+    name: '20250601_173010',
+  },
+  {
+    up: migration_20250605_182240.up,
+    down: migration_20250605_182240.down,
+    name: '20250605_182240'
   },
 ];
