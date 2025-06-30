56449a43e57ee3b129c5306c4a0c56505790bf11
Maypher <maypheroffcial@gmail.com>
2025-06-30 18:22:38 -0400

    Add metadata

--- src/app/(frontend)/[locale]/projects/page.tsx
+++ src/app/(frontend)/[locale]/projects/page.tsx
@@ -4,12 +4,16 @@ import Carousel from '@/components/carousel'
 import { Suspense } from 'react'
 import { draftMode } from 'next/headers'
 import RefreshRouteOnSave from '@/components/admin/RefreshRouteOnSave'
+import { Metadata } from 'next'
+import { getTranslations } from 'next-intl/server'
+import { Media } from '@/payload-types'
 
 export const dynamic = 'error'
 
-export default async function Page() {
+export default async function Page({ params }: { params: Promise<{ locale: 'es' | 'en' }> }) {
   const { isEnabled } = await draftMode()
   const payload = await getPayload({ config })
+  const { locale } = await params
 
   const projects = await payload.find({
     collection: 'project',
@@ -18,6 +22,7 @@ export default async function Page() {
       thumbnail: true,
       _status: true,
     },
+    locale,
     draft: isEnabled,
     overrideAccess: isEnabled,
   })
@@ -31,4 +36,35 @@ export default async function Page() {
       {isEnabled && <RefreshRouteOnSave />}
     </Suspense>
   )
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>
+}): Promise<Metadata> {
+  const { locale } = await params
+
+  const t = await getTranslations({ locale })
+  const title = t('NavBar.projects')
+
+  const payload = await getPayload({ config })
+
+  const data = await payload.find({
+    collection: 'project',
+    locale: locale as 'es' | 'en',
+    select: {
+      thumbnail: true,
+    },
+  })
+
+  const ogImage = data.docs.at(0)?.thumbnail as Media
+
+  return {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_PAYLOAD_URL || ''),
+    title,
+    openGraph: {
+      images: ogImage.url!,
+    },
+  }
 }
