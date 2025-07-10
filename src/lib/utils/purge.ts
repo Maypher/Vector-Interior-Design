ef821ab1e33d7bf877957328edc2e9d088f33080
Maypher <maypheroffcial@gmail.com>
2025-07-10 13:58:00 -0400

    Add debug purge

--- src/lib/utils/purge.ts
+++ src/lib/utils/purge.ts
@@ -5,5 +5,8 @@ const purgePath = 'http://nginx:1810/purge'
 /**Purges the given path from the nginx cache*/
 export default async function purgeRoute(path: string) {
   if (path.startsWith('/')) throw new SyntaxError(`path "${path}" cannot be absolute.`)
-  await fetch(`${purgePath}/${path}`)
+  const res = await fetch(`${purgePath}/${path}`)
+
+  console.log(res)
+  console.log(res.body)
 }
