ab09c5b745ff232dbd41623e4535f1a48faa4b67
Maypher <maypheroffcial@gmail.com>
2025-07-14 22:32:33 -0400

    Change comment

--- src/lib/utils/purge.ts
+++ src/lib/utils/purge.ts
@@ -2,7 +2,7 @@
 
 const purgePath = 'http://varnish'
 
-/**Purges the given path from the nginx cache*/
+/**Purges the given path from the varnish cache*/
 export default async function purgeRoute(path: string) {
   if (path.startsWith('/')) throw new SyntaxError(`path "${path}" cannot be absolute.`)
   await fetch(`${purgePath}/${path}`)
